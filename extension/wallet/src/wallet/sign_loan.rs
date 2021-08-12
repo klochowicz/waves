7491a08c1a23dbc33aa44d7941444c644c422195
rishflab <rishflab@hotmail.com>
2021-08-12 11:56:09 +1000

    Use wallet from baru

--- extension/wallet/src/wallet/sign_loan.rs
+++ extension/wallet/src/wallet/sign_loan.rs
@@ -1,16 +1,18 @@
-use baru::{loan::Borrower1, swap::sign_with_key};
-use elements::{secp256k1_zkp::SECP256K1, sighash::SigHashCache, Transaction};
+use baru::loan::Borrower1;
+use elements::Transaction;
 use futures::lock::Mutex;
 
 use crate::{
+    esplora::EsploraClient,
     storage::Storage,
-    wallet::{current, get_txouts, LoanDetails},
+    wallet::{current, LoanDetails},
     Wallet,
 };
 
 pub(crate) async fn sign_loan(
     name: String,
     current_wallet: &Mutex<Option<Wallet>>,
+    client: &EsploraClient,
 ) -> Result<Transaction, Error> {
     let storage = Storage::local_storage().map_err(Error::Storage)?;
     let borrower = storage
@@ -21,46 +23,10 @@ pub(crate) async fn sign_loan(
         serde_json::from_str::<(Borrower1, LoanDetails)>(&borrower).map_err(Error::Deserialize)?;
 
     let loan_transaction = borrower
-        .sign(|mut transaction| async {
-            let wallet = current(&name, current_wallet).await?;
-            let txouts = get_txouts(&wallet, |utxo, txout| Ok(Some((utxo, txout)))).await?;
-
-            let mut cache = SigHashCache::new(&transaction);
-            let witnesses = transaction
-                .clone()
-                .input
-                .iter()
-                .enumerate()
-                .filter_map(|(index, input)| {
-                    txouts
-                        .iter()
-                        .find(|(utxo, _)| {
-                            utxo.txid == input.previous_output.txid
-                                && utxo.vout == input.previous_output.vout
-                        })
-                        .map(|(_, txout)| (index, txout))
-                })
-                .map(|(index, output)| {
-                    // TODO: It is convenient to use this import, but
-                    // it is weird to use an API from the swap library
-                    // here. Maybe we should move it to a common
-                    // place, so it can be used for different
-                    // protocols
-                    let script_witness = sign_with_key(
-                        SECP256K1,
-                        &mut cache,
-                        index,
-                        &wallet.secret_key,
-                        output.value,
-                    );
-
-                    (index, script_witness)
-                })
-                .collect::<Vec<_>>();
-
-            for (index, witness) in witnesses {
-                transaction.input[index].witness.script_witness = witness
-            }
+        .sign(|transaction| async {
+            let mut wallet = current(&name, current_wallet).await?;
+            wallet.sync(&*client).await?;
+            let transaction = wallet.sign(transaction);
 
             Ok(transaction)
         })
